9dbf992d59b58acd91706fb6ea42d4c18585ee02
Ruslan <tagizaderuslan25@gmail.com>
2024-06-29 23:52:55 +0400

    Updated gitignore

--- Ecommerce/Core/Ecommerce.Domain/Common/EntityBase.cs
+++ Ecommerce/Core/Ecommerce.Domain/Common/EntityBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.Domain.Common
 {
-    public class EntityBase : IEntityBase
+    public abstract class EntityBase : IEntityBase
     {
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
